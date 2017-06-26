5a0698541620a850972bcfdd1a06f0c47a3ca4aa
sveradev <Bastian_Vera@yahoo.com.ar>
2017-06-26 18:10:37 -0300

    security test

--- src/main/java/com/talgham/demo/MvcConfig.java
+++ src/main/java/com/talgham/demo/MvcConfig.java
@@ -1,4 +1,4 @@
-package configuration;
+package com.talgham.demo;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/com/talgham/demo/WebSecurityConfig.java
+++ src/main/java/com/talgham/demo/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package configuration;
+package com.talgham.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
