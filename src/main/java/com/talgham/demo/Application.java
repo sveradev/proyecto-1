efd2f4cd282def2ce284a73e99f31d685d5ddd6a
sveradev <Bastian_Vera@yahoo.com.ar>
2017-06-26 16:14:15 -0300

    Security Test

--- src/main/java/com/talgham/demo/Application.java
+++ src/main/java/com/talgham/demo/Application.java
@@ -16,5 +16,5 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
-
+    
 }
