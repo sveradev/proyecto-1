6f2ea9909d4163ed06061aa4b36ffb5eec15fa6a
sveradev <Bastian_Vera@yahoo.com.ar>
2017-06-27 01:03:53 -0300

    correcciones

--- src/main/java/com/talgham/demo/MvcConfig.java
+++ src/main/java/com/talgham/demo/MvcConfig.java
@@ -9,13 +9,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("login");
+		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/hello").setViewName("hello");
 		registry.addViewController("/login").setViewName("login");
 		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/home").setViewName("home");
 	}
-	
 }
-
-
-
