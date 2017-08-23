fefb2599315cc4c2b2495689c855746e26b55f97
sveradev <Bastian_Vera@yahoo.com.ar>
2017-08-23 13:13:02 -0300

    Update and rename src/main/java/com/talgham/demo/serviceImpl/EstadoServiceImpl.java to src/main/java/com/talgham/demo/service/EstadoService.java

--- src/main/java/com/talgham/demo/service/EstadoService.java
+++ src/main/java/com/talgham/demo/service/EstadoService.java
@@ -1,14 +1,13 @@
-package com.talgham.demo.serviceImpl;
+package com.talgham.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.talgham.demo.service.EstadoService;
 import com.talgham.demo.model.Estado;
 import com.talgham.demo.repository.EstadoRepository;
 
 @Component
-public class EstadoServiceImpl implements EstadoService {
+public class EstadoService {
 	
 	@Autowired
 	private EstadoRepository estadoRepository;
