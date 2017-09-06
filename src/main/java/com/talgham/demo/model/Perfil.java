a96d1ced7caa53e387f9304d95144156f0a757fb
sveradev <Bastian_Vera@yahoo.com.ar>
2017-09-06 12:11:37 -0300

    Create Perfil.java

---
+++ src/main/java/com/talgham/demo/model/Perfil.java
@@ -0,0 +1,39 @@
+ package com.talgham.demo.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.talgham.demo.common.Constantes;
+@Entity
+public class Perfil {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String nombre;
+	private String cuit;
+  
+	private String descripcion;
+  	private Date cierreEjercicio;
+	private Date fechaAlta;
+	private Date fechaModificado;
+	private Date fechaBaja;
+  
+ 	@ManyToOne @JoinColumn(name="usuario_id")
+	private Usuario representante;
+	@ManyToOne @JoinColumn(name="usuario_id")
+	private Usuario contador;
+  
+	public Long getId(){
+		return this.id;
+	}
+	public void setId(Long id){
+		this.id = id;
+	}
+}
