49a9f3f7251182c0b0e00feb4d5dec1206dff42f
sveradev <Bastian_Vera@yahoo.com.ar>
2017-09-06 12:10:36 -0300

    Update Solicitud.java

--- src/main/java/com/talgham/demo/model/Solicitud.java
+++ src/main/java/com/talgham/demo/model/Solicitud.java
@@ -18,92 +18,16 @@ public class Solicitud {
 	private Long id;
 	private String nombre;
 	private String titulo;
-	private String email;
+	@ManyToOne @JoinColumn(name="trabajo_id")
+	private Trabajo trabajo;
 	private String descripcion;
+	
+	private Date fechaCreacion;
 	private Date fechaSolicitado;
 	private Date fechaModificado;
 	private Date fechaFinalizado;
 	@ManyToOne @JoinColumn(name="estado_id")
 	private Estado estado;
 	@ManyToOne @JoinColumn(name="usuario_id")
-	private Usuario responsable;
-
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDescripcion() {
-		return descripcion;
-	}
-	public void setDescripcion(String descripcion) {
-		this.descripcion = descripcion;
-	}
-	public Date getFechaSolicitado() {
-		return fechaSolicitado;
-	}
-	public void setFechaSolicitado(Date fechaSolicitado) {
-		this.fechaSolicitado = fechaSolicitado;
-	}
-	public Date getFechaModificado() {
-		return fechaModificado;
-	}
-	public void setFechaModificado(Date fechaModificado) {
-		this.fechaModificado = fechaModificado;
-	}
-	public Date getFechaFinalizado() {
-		return fechaFinalizado;
-	}
-	public void setFechaFinalizado(Date fechaFinalizado) {
-		this.fechaFinalizado = fechaFinalizado;
-	}
-	public Estado getEstado() {
-		return estado;
-	}
-	public void setEstado(Estado estado) {
-		this.estado = estado;
-	}
-	public Usuario getResponsable() {
-		return responsable;
-	}
-	public void setResponsable(Usuario responsable) {
-		this.responsable = responsable;
-	}
-
-	private Date addDays (Date date,Integer days){
-		Long result = date.getTime() + (1000 * 60 * 60 * 24 * days);
-		return new Date(result);
-	}
-	
-	public Boolean isSuccess() {
-		return estado.getId() == Constantes.ESTADO_SOLICITADO && new Date().before(addDays(fechaSolicitado, 7));
-	}
-	public Boolean isPending() {
-		return estado.getId() == Constantes.ESTADO_SOLICITADO && new Date().after(addDays(fechaSolicitado, 7));
-	}
-	public Boolean isOverdue() {
-		return estado.getId() == Constantes.ESTADO_SOLICITADO && new Date().after(addDays(fechaSolicitado, 15));
-	}
-	public Boolean isActive() {
-		return estado.getId() != Constantes.ESTADO_SOLICITADO && estado.getId() != Constantes.ESTADO_FINALIZADO;
-	}
+	private Cliente cliesnte;
 }
