c29cb92fa1b8af2049e1807b817f51edd9adaa62
sveradev <Bastian_Vera@yahoo.com.ar>
2017-09-06 12:00:29 -0300

    Create trabajo.java

---
+++ src/main/java/com/talgham/demo/model/trabajo.java
@@ -0,0 +1,21 @@
+package com.talgham.demo.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class trabajo {
+	
+	@Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String nombre;
+	private String descripcion;
+	private Date fechaCreacion;
+	private Date fechaBaja;
+
+}
