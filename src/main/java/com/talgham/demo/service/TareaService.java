08c92de37c1c7e92e273791640b363d6edb9d67d
svera <svera@svera.boldt.com.ar>
2017-09-13 15:22:58 -0300

    13/09 nueva tarea programada - envio de Reporte mensual.

--- src/main/java/com/talgham/demo/service/TareaService.java
+++ src/main/java/com/talgham/demo/service/TareaService.java
@@ -1,18 +1,22 @@
 package com.talgham.demo.service;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.databind.ser.std.StdKeySerializers.Default;
 import com.talgham.demo.common.Constantes;
+import com.talgham.demo.model.Email;
 import com.talgham.demo.model.Programa;
 import com.talgham.demo.model.Solicitud;
 
@@ -23,15 +27,16 @@ public class TareaService {
 	private ProgramaService programaService;
 	@Autowired
 	private SolicitudService solicitudService;
-	
+	@Autowired
+	private EmailService emailService;
 	
 	private static final Logger log = LoggerFactory.getLogger(TareaService.class);
 	
-	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+	private static final SimpleDateFormat formatterTime = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
 	
 	@Scheduled(fixedRate = 24*60*60*1000)
 	public void crearSolicitudProgramada() {
-		log.info("Inicio de creacion de solicitudes programadas - {}", dateFormat.format(new Date()));
+		log.info("Inicio de creacion de solicitudes programadas - {}", formatterTime.format(new Date()));
 
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.DAY_OF_MONTH, 1);
@@ -70,6 +75,46 @@ public class TareaService {
 			programa.setFechaUltimo(solicitud.getFechaSolicitado());
 			programaService.guardar(programa);
 		}
-		log.info("Finalizó la creacion de solicitudes programadas - {}", dateFormat.format(new Date()));
+		log.info("Finalizó la creacion de solicitudes programadas - {}", formatterTime.format(new Date()));
+	}
+	
+	@Scheduled(fixedRate = 24*60*60*1000)
+	public void enviarSolicitudReporte() throws ParseException {
+		
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.DAY_OF_MONTH, 1);
+	    Date fechaDesde = c.getTime();
+		    
+		Calendar c2 = Calendar.getInstance();
+		c2.set(Calendar.DAY_OF_MONTH, c2.getActualMaximum(Calendar.DAY_OF_MONTH));
+		Date fechaHasta = c2.getTime();
+		
+		List<Solicitud> solicitudes = (List<Solicitud>) solicitudService.buscarPorFechas(fechaDesde, fechaHasta);
+		ModelAndView emailResult = new ModelAndView("EmailReporte");
+		emailResult.addObject("solicitudes",solicitudes);
+
+		//Envio de mail.
+		Email emailTemplate = emailService.buscarPorActividad(Constantes.ACTIVIDAD_REPOTAR_SOLICITUD);
+
+		if(emailTemplate != null){
+			String to = emailTemplate.getDireccion();
+			String subject = emailTemplate.getSubject();
+		
+			//Ruta completa al template html
+			String templateHtml = "src/main/resources/emailTemplates/emailReporte.html";
+			
+			//Mapa de objetos a renderizar (clave elemento mustache, valor opbejto)
+			Map<String, Object> data = new HashMap<String, Object>();
+			data.put("solicitudes", solicitudes);
+			
+			String templateCompilado = EmailService.getEmailTemplate(templateHtml, data);
+			 
+			try {
+				emailService.sendEmail(to, subject, templateCompilado);
+			} catch (Exception e) {
+				e.printStackTrace();
+				log.error("Ha ocurrido un error al enviar el email del reporte de solicitudes.");
+			}
+		}
 	}
 }
